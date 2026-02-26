da884d830e7a8ad29cf4d673ea68b64abf0dda47
Candeğer Şen <157699864+candegersen@users.noreply.github.com>
2026-02-26 05:51:08 +0300

    Add files via upload

---
+++ UnRedemption.cs
@@ -0,0 +1,27 @@
+static List<string> GetHollywoodFigures()
+{
+    return new List<string>
+    {
+    "Laura Prepon",
+    "Natalie Dormer",
+    "Ricky Gervais",
+    "Netflix",
+    "Can Evrenol"
+  };
+}
+
+static List<string> KnowTheBenefitsOfSoftware() {
+     return new List<string>
+    {
+    "Pushes logic",
+    "Collects juice",
+    "Keeps you away from fakes",
+    "Makes you a global citizen",
+    "Protects your heritage",
+    "Keeps you grope free"
+  };
+}
+
+static string KeepsIntelligenceSafe(){
+    return "Protect your IQ and give some EQ to the media folks. Not to be confused with Netflix.";
+}
